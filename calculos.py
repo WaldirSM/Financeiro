9ea9a60d46142a9903e2fb5bbd3aa956e0045ca3
waldir <waldirsiqueira@ymail.com>
2018-02-21 14:51:45 -0300

    versao1

--- calculos.py
+++ calculos.py
@@ -1,1 +1,7 @@
-trtr
+def soma (salario1, dependente1):
+    return salario1 + dependente1
+
+
+print soma (salario1 + dependente1)
+
+"""Soma da renda da provedor e seus dependentes para cálculo de imposto de renda"""
